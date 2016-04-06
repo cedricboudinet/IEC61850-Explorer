62abf00891e120fa2ec4017d8c9906b3988be846
Cedric Boudinet <boudinpg@gmail.com>
2016-04-06 12:15:19 +0200

    Adding test server

---
+++ iec61850_testsrv.c
@@ -0,0 +1,74 @@
+/*
+ * test de serveur iec61850
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <libiec61850/iec61850_server.h>
+#include <libiec61850/hal_thread.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <pthread.h>
+
+static int running = 0;
+static IedServer iedServer = NULL;
+
+void sigint_handler(int signalId)
+{
+	running = 0;
+}
+
+int launchIedServer(int port_61850)
+{
+	int i;
+	time_t currTime;
+	currTime=time(NULL);
+	printf("Starting IED server %s", ctime(&currTime));
+	IedModel* myModel = IedModel_create("test");
+	LogicalDevice* lDevice1 = LogicalDevice_create("Device", myModel);
+	LogicalNode* lln0 = LogicalNode_create("LLN0", lDevice1);
+	char buffer[16];
+	for(i=0;i<3;i++)
+	{
+		sprintf(buffer, "varASG%d", i);
+		CDC_ASG_create(buffer, (ModelNode*) lln0, 0, false);
+		sprintf(buffer, "varSAV%d", i);
+		CDC_SAV_create(buffer, (ModelNode*) lln0, 0, false);
+	}
+	iedServer = IedServer_create(myModel);
+
+	IedServer_start(iedServer, port_61850);
+	if (!IedServer_isRunning(iedServer)) {
+		fprintf(stderr, "Starting IED server failed! Exit.\n");
+		IedServer_destroy(iedServer);
+		exit(-1);
+	}
+	printf("Iec61850 test server is ready\n");
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	int port_61850 = 102;
+	int i;
+	if (argc > 1)
+		port_61850 = atoi(argv[1]);
+	running = 1;
+	signal(SIGINT, sigint_handler);
+	launchIedServer(port_61850);
+
+	while (running) 
+	{
+		sleep(1);
+	}
+
+	printf("Stoping iec server\n");
+	IedServer_stop(iedServer);
+	IedServer_destroy(iedServer);
+
+	return (0);
+}
+

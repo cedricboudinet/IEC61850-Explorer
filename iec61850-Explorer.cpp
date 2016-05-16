13d3eb797f38b19de51c28a5f0ba8079c2b0f564
Cedric Boudinet <boudinpg@gmail.com>
2016-05-16 09:34:39 +0200

    Adding icon to main window

--- iec61850-Explorer.cpp
+++ iec61850-Explorer.cpp
@@ -1,10 +1,13 @@
 #include "ExplorerWindow.h"
 #include <QApplication>
-
+#include <QIcon>
 
 int main(int argc, char **argv)
 {
 	QApplication app (argc, argv);
+	QIcon appIcon;
+	appIcon.addFile(":/icons/spy");
+	app.setWindowIcon(appIcon);
 	ExplorerWindow window;
 	window.show();
 	return app.exec();

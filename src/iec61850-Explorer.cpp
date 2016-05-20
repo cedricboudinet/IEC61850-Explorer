b8a5baabdead2dce365895a73bc1ea7302d91cfd
Cedric Boudinet <boudinpg@gmail.com>
2016-05-20 07:23:31 +0200

    Project tree reorganisation

--- src/iec61850-Explorer.cpp
+++ src/iec61850-Explorer.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
 	QApplication app (argc, argv);
 	QIcon appIcon;
-	appIcon.addFile(":/icons/spy");
+	appIcon.addFile(":/icons/art/spy");
 	app.setWindowIcon(appIcon);
 	ExplorerWindow window;
 	window.show();
